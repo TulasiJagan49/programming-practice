0126a71249b7bc63f9504172a312735ac83ca3c0
TulasiJagan49 <jaganmeruga777@msitprogram.net>
2020-06-08 14:04:45 +0530

    Small Changes made in making this as an to-do application.

--- web-programming-practice/js/script.js
+++ web-programming-practice/js/script.js
@@ -1,2 +1,13 @@
+let name = prompt("What is your name?");
 let d = new Date();
-document.body.innerHTML = "<h1> Today's date is " + d + "</h1>"
+let helloString = `Hello ${name}`
+
+let heading = document.getElementById("heading")
+heading.insertAdjacentHTML('beforeend', "<h1> Today's date is " + d.getDate() + " - " + d.getMonth() + " - " + d.getFullYear() + "</h1>");
+
+let p = document.createElement("p");
+let t = document.createTextNode(`Hello, ${name}`);
+p.appendChild(t)
+heading.appendChild(p);
+
+let todo_list = []
